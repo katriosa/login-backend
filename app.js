d71ecbe4d103a66ce007c71b5aadea55bbb26c30
katriosa <ee.chistyakova@gmail.com>
2025-04-01 15:18:40 +0200

    use cors instead of manual setting headers

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import express from "express";
+import cors from "cors";
 import { createUser, login } from "./auth.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.post("/signup", async (req, res) => {
   try {
